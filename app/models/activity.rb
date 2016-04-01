1ceab547de71ba0c81f25a2cdd29989ecc9213e8
Nguyen Thanh Luan <nguyen.thanh.luan@framgia.com>
2016-04-01 11:23:20 +0700

    Activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,27 @@
 class Activity < ActiveRecord::Base
   belongs_to :user
+
+  enum action_type: {examination_activities: 0, question_activities: 1}
+
+  scope :can_see_by_user, ->(user){
+    where user_id: User.can_manage(user)
+  }
+
+  def object
+    return Examination.find_by id: target_id if examination_activities?
+    return Question.find_by id: target_id if question_activities?
+  end
+
+  def action
+    return "Examination" if examination_activities?
+    return "Contribute question"if question_activities?
+  end
+
+  def object_name
+    object.category.name if object.present?
+  end
+
+  def object_status
+    object.status if object.present?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ActiveRecord::Base
   has_many :questions, dependent: :destroy
 
   enum role: {normal: 0, admin: 1, mod: 2}
+
+  scope :can_manage, ->(user){
+    return where("") if user.admin?
+    return where(id: [User.where(role: "normal").ids, user.id]) if user.mod?
+    return where(id: user.id) if user.normal?
+    }  
+
+  def feed
+    return Activity.can_see_by_user(self).order "created_at desc"
+  end
 end
